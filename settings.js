723fb63cbc7d09694b1a92431cf07d427b84e41b
shanachanpanel <126304667+shanachanpanel@users.noreply.github.com>
2023-09-24 22:03:51 +0700

    Update settings.js by record shanachan

--- settings.js
+++ settings.js
@@ -2,7 +2,35 @@ const fs = require('fs')
 
 global.creator = 'shanachan' 
 global.apikey = ["shanachan", "riza", "rizasuamishana"]
-
+global.MONGO_DB_URI = "mongodb+srv://boyahmad13131:X^:Bb^+q^zRA4TC@cluster8.cobowvl.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.your_email = "rizaedtz@gmail.com" //email
+global.email_password = "ttyy xwla apgo ckvk" //application password email
+global.limitCount = 10000
+global.YUOR_PORT = 8000
+global.loghandler = {
+	noapikey:{
+		status: 403,
+        message: 'Input parameter apikey',
+        creator: `${creator}`,
+        result: "error"
+    },
+    error: {
+        status: 503,
+        message: 'Service Unavaible, Sedang dalam perbaikan',
+        creator: `${creator}`
+    },
+    apikey: {
+    	status: 403,
+    	message: 'Forbiden, Invalid apikey',
+    	creator: `${creator}`
+    },
+    noturl: {
+    	status: 403,
+    	message: 'Forbiden, Invlid url, masukkan parameter url',
+    	creator: `${creator}`,
+    }
+}
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
